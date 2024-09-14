3e10c9668a275ec3484cf771120e9dc448f29df3
k.grajek <k.grajek@insys.pl>
2024-09-14 19:55:03 +0200

    first version

---
+++ src/scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+const SPEED = 5.0
+const ROTATION_SPEED = 1.5
+@export var camera_node : Node3D
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	var input_dir = Input.get_vector("rotate_left", "rotate_right", "move_front", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if Input.is_action_pressed("rotate_left"):
+		rotate_y(ROTATION_SPEED * delta)
+	elif Input.is_action_pressed("rotate_right"):
+		rotate_y(-ROTATION_SPEED * delta)
+	move_and_slide()
