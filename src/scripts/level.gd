50d8db25f9fc6a5d7dfbd49acaa1c70a59d3ee30
k.grajek <k.grajek@insys.pl>
2024-09-15 20:12:26 +0200

    added roe; setted terrain to y0

--- src/scripts/level.gd
+++ src/scripts/level.gd
@@ -1,6 +1,27 @@
 extends Node3D
-var utils = preload("res://scripts/utils.gd")  
+var utils = preload("res://scripts/utils.gd")
+
+func _ready():
+	reset_obstacles()
+
 func _on_player_custom_position_reseted():
-	$ObstaclesHolder/Obstacle01.position.x = utils.get_random_value(5)
-	$ObstaclesHolder/Obstacle02.position.x = utils.get_random_value(5)
-	$ObstaclesHolder/StartSign.scale = Vector3.ZERO
+	$StartSign.scale = Vector3.ZERO
+	reset_obstacles()
+
+func reset_obstacles():
+	var picked_obstacle = randi_range(0, 1)
+
+	if picked_obstacle == 0:
+		$Randomizer/Sarna.position.x = utils.get_random_value(5)
+	elif picked_obstacle == 1:
+		$Randomizer/ObstaclesHolder/Obstacle01.position.x = utils.get_random_value(5)
+		$Randomizer/ObstaclesHolder/Obstacle02.position.x = utils.get_random_value(5)
+
+	var children = $Randomizer.get_children()
+	for i in range(children.size()):
+		var child = children[i]
+
+		if i == picked_obstacle:
+			child.scale = Vector3.ONE
+		else:
+			child.scale = Vector3.ZERO
