a34ec3e225356d0faf2a67589d2f8fed765c97b8
hejzzy <hejzzy@gmail.com>
2024-09-16 17:31:06 +0200

    Add files via upload

---
+++ src/scripts/main_menu.gd
@@ -0,0 +1,9 @@
+class_name main_menu
+extends Control
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+func _on_settings_pressed() -> void:
+	$".".hide()
+	$"../SettingsMenu".show()
+func _on_exit_pressed() -> void:
+	get_tree().quit()
