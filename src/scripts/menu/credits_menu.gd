9fd81bfc106f59077bd1e4dd42df89b02d8d648b
hejzzy <hejzzy@gmail.com>
2024-10-07 19:09:13 +0200

    .

--- src/scripts/menu/credits_menu.gd
+++ src/scripts/menu/credits_menu.gd
@@ -3,4 +3,6 @@ func _ready() -> void:
 	Engine.time_scale = 1
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 func _on_back_pressed() -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
--- src/scripts/object/transition_screen.gd
+++ src/scripts/object/transition_screen.gd
@@ -1,16 +1,16 @@
 extends CanvasLayer
-signal on_transition_finished
+signal  on_transition_finished
 @onready var color_rect = $ColorRect
 @onready var animation_player = $AnimationPlayer
-func _ready() -> void:
+func _ready():
 	color_rect.visible = false
 	animation_player.animation_finished.connect(_on_animation_finished)
 func _on_animation_finished(anim_name):
-	if anim_name == "animation1":
+	if anim_name == "1":
 		on_transition_finished.emit()
-		animation_player.play("animation2")
-	elif anim_name == "animation2":
+		animation_player.play("2")
+	elif anim_name == "2":
 		color_rect.visible = false
 func transition():
-	color_rect.visible = false
-	animation_player.play("animation1")
+	color_rect.visible = true
+	animation_player.play("1")
