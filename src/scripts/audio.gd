dfe5e13b5808af73a4ed43dbeccc1da324f83f6c
hejzzy <hejzzy@gmail.com>
2024-10-08 21:11:18 +0200

    Delete src directory

--- src/scripts/audio.gd
+++
@@ -1,9 +0,0 @@
-extends TabBar
-func _on_master_off_pressed() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -80)
-func _on_master_on_pressed() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
-func _on_sfx_on_pressed() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), 0)
-func _on_sfx_off_pressed() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), -80)
--- src/scripts/graphics.gd
+++
@@ -1,5 +0,0 @@
-extends TabBar
-func _on_maximized_pressed() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
-func _on_fullscreen_pressed() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
