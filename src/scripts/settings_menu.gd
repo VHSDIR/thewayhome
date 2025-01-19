dd6a9b29f744a42e582f8c35b66994855f9dedcc
hejzzy <hejzzy@gmail.com>
2025-01-19 19:01:34 +0100

    .

--- src/scripts/settings_menu.gd
+++ src/scripts/settings_menu.gd
@@ -3,6 +3,4 @@ func _ready() -> void:
 	Engine.time_scale = 1
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 func _on_back_pressed() -> void:
-	TransitionScreen.transition()
-	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
