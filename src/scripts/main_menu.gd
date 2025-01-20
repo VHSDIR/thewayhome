9fda4f0ec4bab477fa29e94a44d66151205a19a7
hejzzy <hejzzy@gmail.com>
2025-01-20 10:47:36 +0100

    .

--- src/scripts/main_menu.gd
+++ src/scripts/main_menu.gd
@@ -3,16 +3,12 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	Music.play_music_level()
 	Engine.time_scale = 1
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
 func _on_more_games_pressed() -> void:
-	OS.shell_open("https://hejzzy.github.io")
-
+	OS.shell_open("https://gkppgames.github.io")
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("escape_key"):
 		get_tree().quit()
